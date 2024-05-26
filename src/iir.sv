5f0555422dded3a1b9342d926d6be1d8ba65d4c0
Fernandez Martinez Luis <luis.fernandez.martinez98@gmail.com>
2024-05-26 18:49:24 +0200

    new filter formula

--- src/iir.sv
+++ src/iir.sv
@@ -8,20 +8,24 @@ module iir
         output signed[15:0] data_o
     ); 
 
-    logic signed[15:0] delayed_input_q, delayed_input_d;
+    logic signed[15:0] delayed_input_q, delayed_input_d, delayed_output_q;
     logic signed[15:0] sum;
+    logic signed[15:0] factor = 1;
         
     always_ff @(posedge clk_i) begin
         if (reset_i) begin
             delayed_input_q <= 0;
+            delayed_output_q <= 0;
         end else begin
             delayed_input_d <= data_i;
             delayed_input_q <= delayed_input_d;
+            delayed_output_q <= sum;
         end
     end
 
     always_comb begin
-        sum = (- delayed_input_q * timeconstant + (1 + timeconstant) * data_i);
+        //sum = (- delayed_input_q * timeconstant + (1 + timeconstant) * data_i);
+        sum = (- delayed_input_q * (1-2*timeconstant) + (1 + 2*timeconstant) * data_i)*factor - delayed_output_q;
     end
 
     assign data_o = sum;
